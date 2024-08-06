c101e82a489b200182c84909161ba9431b00807e
wanghanlin <wanghanlinlin@126.com>
2024-08-06 20:25:01 +0800

    添加授权登录配置

---
+++ src/main/java/com/aurorapixel/cortexai/api/controller/auth/AuthController.java
@@ -0,0 +1,13 @@
+package com.aurorapixel.cortexai.api.controller.auth;
+
+
+import com.aurorapixel.cortexai.annotation.CortexAIController;
+import lombok.AllArgsConstructor;
+
+/**
+ * 三方授登陆
+ */
+@CortexAIController("/auth")
+@AllArgsConstructor
+public class AuthController {
+}
---
+++ src/main/java/com/aurorapixel/cortexai/common/config/auth/SecurityConfig.java
@@ -0,0 +1,28 @@
+package com.aurorapixel.cortexai.common.config.auth;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig  {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/login").permitAll()
+                        .anyRequest().authenticated()
+                );
+
+        http.addFilterBefore(null, UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+    }
+}
